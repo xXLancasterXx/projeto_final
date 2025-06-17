4f4a3b321ad01d53beadc989cbd4c19e3d89d67e
Pedro Lucas <pedrolucas_vasconcelos@hotmail.com>
2025-06-17 18:24:10 -0300

    add initial utils.py file for database setup

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template, request
 from flask_migrate import Migrate
-from database import db
+from utils import db
 from usuarios import Usuario
 from fichas import Fichas
+import os
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'elegebete'
-conexao = 'sqlite:///banco.db'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+db_usuario = os.getenv('DB_USERNAME')
+db_senha = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_mydb = os.getenv('DB_DATABASE')
+
+conexao = f"mysql+pymysql://{db_usuario}:{db_senha}@{db_host}/{db_mydb}"
 app.config['SQLALCHEMY_DATABASE_URI'] = conexao
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
