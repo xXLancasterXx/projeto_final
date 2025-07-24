1b37119942506171d942be3b7f01d000d9b7a8a0
Pedro Lucas <pedrolucas_vasconcelos@hotmail.com>
2025-07-24 14:11:44 -0300

    MVP PARCIALEMENTE CONCLUIDO

--- fichas.py
+++ fichas.py
@@ -3,6 +3,11 @@ from utils import db
 class Fichas(db.Model):
     __tablename__ = 'fichas'
     id = db.Column(db.Integer, primary_key=True)
+    dono = db.Column(
+    db.Integer,
+    db.ForeignKey('usuarios.id', name='fk_fichas_dono'),
+    nullable=False
+    )
     personagem = db.Column(db.String(80), nullable=False)
     classe = db.Column(db.String(80), nullable=False)
     nivel = db.Column(db.Integer, nullable=False)
@@ -12,8 +17,9 @@ class Fichas(db.Model):
     constituicao = db.Column(db.Integer, nullable=False)
     inteligencia = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, personagem, classe, nivel, raca, forca, destreza, constituicao, inteligencia):
+    def __init__(self, personagem, dono, classe, nivel, raca, forca, destreza, constituicao, inteligencia):
         self.personagem = personagem
+        self.dono = dono
         self.classe = classe
         self.nivel = nivel
         self.raca = raca
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask import redirect, flash
+from flask import redirect, flash, session
 from flask_migrate import Migrate
 from utils import db
 from usuarios import Usuario
@@ -33,10 +33,17 @@ def login():
 
         
         if Usuario.query.filter_by(userName=username, password=password).first():
-            return render_template('index.html', username=username)
+            session['user_id'] = Usuario.query.filter_by(userName=username).first().id
+            fichas = Fichas.query.filter_by(dono=session['user_id']).all()
+            return render_template('index.html', username=username, fichas=fichas)
         else:
             flash('Usuário ou senha inválidos.')
             return redirect('/')
+
+@app.route('/home')
+def home():
+    fichas = Fichas.query.filter_by(dono=session.get('user_id')).all()
+    return render_template('index.html', fichas=fichas)
 
 @app.route('/registro')
 def registro():
@@ -60,6 +67,61 @@ def register():
             flash('Usuário registrado com sucesso!')
             return redirect('/')
 
+
+@app.route('/criar_ficha', methods=['GET', 'POST'])
+def criar_ficha():
+    if request.method == 'POST':
+        dono = session.get('user_id')  # aqui sim!
+        if not dono:
+            flash("Você precisa estar logado para criar uma ficha.")
+            return redirect('/')
+        personagem = request.form['personagem']
+        classe = request.form['classe']
+        nivel = int(request.form['nivel'])
+        raca = request.form['raca']
+        forca = int(request.form['forca'])
+        destreza = int(request.form['destreza'])
+        constituicao = int(request.form['constituicao'])
+        inteligencia = int(request.form['inteligencia'])
+
+        nova_ficha = Fichas(
+            personagem=personagem,
+            dono=dono,
+            classe=classe,
+            nivel=nivel,
+            raca=raca,
+            forca=forca,
+            destreza=destreza,
+            constituicao=constituicao,
+            inteligencia=inteligencia
+        )
+
+        # Atribuir dono se você usa autenticação
+        nova_ficha.dono = session.get('user_id')  # ou o id do usuário logado
+
+        db.session.add(nova_ficha)
+        db.session.commit()
+        flash("Ficha criada com sucesso!")
+        return redirect('/home')
+
+    return render_template('criar_ficha.html')
+
+@app.route('/ficha/<int:id>')
+def ficha(id):
+    ficha = Fichas.query.get_or_404(id)
+    return render_template('ficha.html', ficha=ficha)
+
+
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+
+
+
+
+###############################################
 @app.route('/sobre')
 def sobre():
     return render_template('sobre.html')
